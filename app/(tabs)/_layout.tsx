4bc03b2fb8d584dfa866e0ba1c4f592cd1481c0a
vickapalo <victoria.gyselynck@hotmail.com>
2025-10-16 10:13:56 +0200

    layout redone

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,30 +11,17 @@ export default function TabLayout() {
 
   return (
     <Tabs
+      initialRouteName="calendar"
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
         name="calendar"
         options={{
           title: 'Calendrier',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="calendar" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
     </Tabs>
